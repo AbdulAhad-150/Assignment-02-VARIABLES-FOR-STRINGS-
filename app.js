f8aea2935079562a895fb026e8cc5dfd6fd46c42
AbdulAhad-150 <abdulahad150@gmail.com>
2023-09-30 09:21:59 -0700

    Variables for Strings

---
+++ app.js
@@ -0,0 +1,30 @@
+// Question no 1
+var name = "Ahad";
+alert(name);
+
+// Question no 2
+var myname ="Abdulahad Yaseen"
+alert(myname);
+
+// Question no 3
+var js ="message"
+alert(js)
+
+// Question no 5
+alert("pizza\rpizz\rpiz\rpi\rp");
+
+// Question no 6
+var email ="My email adress is abdulahadyaseen150@gmail.com"
+alert(email);
+
+// Question no 7
+var book ="A smarter way to learn JavaScript"
+alert(book);
+
+// Question no 8
+var message ="Yah! I can write HTML content through JavaScript"
+document.write(message);
+
+// Question no 9
+var symbols ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(symbols);
